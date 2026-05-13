c1f8466732acce0a502a1cd5e62ababb79c7ed98
msanthoshnaik45-boop <msanthoshnaik45@gmail.com>
2026-05-13 11:52:38 +0530

    Add files via upload

---
+++ 1st class.py
@@ -0,0 +1,203 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+print("sansh")
+sansh
+a=25
+b=24
+print(a+b)
+49
+print a/25
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+SyntaxError: invalid syntax
+
+print(a/25)
+1.0
+print("chikiri")
+chikiri
+
+
+print("chikiri/n")
+chikiri/n
+
+
+
+
+
+
+
+
+
+
+
+
+print(2**100)
+1267650600228229401496703205376
+print(2**5)
+32
+print(2**10)
+1024
+
+a=input("enter a Value")
+enter a Value
+print(a)
+
+45
+45
+a=input("enter a Value")
+enter a Value45
+SyntaxError: multiple statements found while compiling a single statement
+print(a)
+
+45
+45
+a=input("enter a value")
+enter a value45
+print(a)
+45
+b=input("enter b value")
+enter b value25
+print(b)
+25
+print(a+b)
+4525
+
+a=int(input("enter a value"))
+enter a value10
+b=int(input("enter a value"))
+enter a value10
+print(a+b)
+20
+
+
+p=float(input("enter principle")
+        100000
+        
+SyntaxError: '(' was never closed
+p=float(input("enter principle"))
+        
+enter principle100000
+t=float(input("enter time"))
+        
+enter time365
+
+==================================================== RESTART: C:/Users/ASUS/OneDrive/Documents/ci.py ====================================================
+enter principle100000
+enter time365
+enter rate2
+Traceback (most recent call last):
+  File "C:/Users/ASUS/OneDrive/Documents/ci.py", line 5, in <module>
+    printf("compound interest=",ci)
+NameError: name 'printf' is not defined. Did you mean: 'print'?
+enter time365
+        
+SyntaxError: invalid syntax
+r=float(input("enter rate"))
+        
+enter rate2
+ci=100000*(1+2/100)**365-100000
+        
+print("compound interest=",ci)
+        
+compound interest= 137640829.19660768
+print(r)
+        
+2.0
+p=float(input("enter principle")
+        100
+        
+SyntaxError: '(' was never closed
+p=float(input("enter principle"))
+        
+enter principle100
+t=float(input("enter time"))
+        
+enter time30
+r=float(input("enter rate"))
+        
+enter rate2
+ci=p*(1+r/100)**t-p
+        
+print("compound interest=",ci)
+        
+compound interest= 81.13615841033547
+
+
+p=float(input("enter principle"))
+        
+enter principle500
+t=float(input("enter time"))
+        
+enter time5
+
+print("sansh")
+        
+sansh
+a=int(input("enter a value"))
+        
+enter a value45
+print(a)
+        
+45
+string name = 'Chaitantya'
+        
+SyntaxError: invalid syntax
+string name = "Chaitanya"
+        
+SyntaxError: invalid syntax
+Name = ("Chaitanya")
+        
+a = ("Chaitanya")
+        
+print (a)
+        
+Chaitanya
+print ("My Name Is ", a)
+...         
+My Name Is  Chaitanya
+>>> 
+>>> 
+>>> a=1
+...         
+>>> b=2
+...         
+>>> c=3
+...         
+>>> print(a+b+c)
+...         
+6
+>>> 
+>>> 
+>>> a,b,c=("25B11CS614","sansh","cse8")
+...         
+>>> print(a)
+...         
+25B11CS614
+>>> print(b)
+...         
+sansh
+>>> print(c)
+...         
+cse8
+>>> 
+>>> a=25b11cs614
+...         
+SyntaxError: invalid decimal literal
+>>> a=("25B11CS614")
+...         
+>>> b=("sansh")
+...         
+>>> c=("cse")
+...         
+>>> 
+>>> print(a)(b)(c)
+...         
+25B11CS614
+Traceback (most recent call last):
+  File "<pyshell#95>", line 1, in <module>
+    print(a)(b)(c)
+TypeError: 'NoneType' object is not callable
+>>> print(a),(b),(c)
+...         
+25B11CS614
+(None, 'sansh', 'cse')
